180217388d8c3fbb043253e0a13f3b74889b5d6a
Aziz Kosber <a.kosber@gmail.com>
2018-06-28 20:27:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,9 @@
 #gets initialized with brand. so probably would set it in initialize then only need an attr_reader
 #has #cobble which puts out a phrase and changes the condition
 class Shoe
+  attr_accessor :color
   attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
